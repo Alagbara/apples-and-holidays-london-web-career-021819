d2d48fa401f2e51a45a3f7bdadd159882a952f9a
Moses Olawoyin <m.bastford@gmail.com>
2019-02-09 00:20:57 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -78,6 +78,11 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
+  
+  holiday_hash.map do |holiday, supplies|
+     if supplies include("BBQ")
+       supplies
+   end
 
 end
 
